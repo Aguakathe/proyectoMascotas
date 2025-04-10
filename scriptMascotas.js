8f579aab9b179dc1424cbff03f94d8729190f175
Aguakathe <sktorres6@gmail.com>
2025-04-10 09:27:14 -0500

    se agregaron estilos de Nosotros y el Script

---
+++ scriptMascotas.js
@@ -0,0 +1,65 @@
+const pets = [
+    {
+        type: 'perro',
+        name: 'Firulais',
+        age: '3 años',
+        gender: 'Macho',
+        image: 'img/perro1.jpg'
+    },
+    {
+        type: 'gato',
+        name: 'Misu',
+        age: '2 años',
+        gender: 'Hembra',
+        image: 'img/gato1.jpg'
+    },
+    {
+        type: 'perro',
+        name: 'Rex',
+        age: '5 años',
+        gender: 'Macho',
+        image: 'img/perro2.jpg'
+    },
+    {
+        type: 'gato',
+        name: 'Luna',
+        age: '1 año',
+        gender: 'Hembra',
+        image: 'img/gato2.jpg'
+    },
+    {
+        type: 'perro',
+        name: 'Max',
+        age: '4 años',
+        gender: 'Macho',
+        image: 'img/perro3.jpg'
+    },
+    {
+        type: 'gato',
+        name: 'Bella',
+        age: '2 años',
+        gender: 'Hembra',
+        image: 'img/gato3.jpg'
+    },
+    {
+        type: 'perro',
+        name: 'Bolt',
+        age: '2 años',
+        gender: 'Macho',
+        image: 'img/perro4.jpg'
+      },
+      {
+        type: 'gato',
+        name: 'Coco',
+        age: '3 años',
+        gender: 'Hembra',
+        image: 'img/gato4.jpg'
+      },
+      {
+        type: 'perro',
+        name: 'Rocky',
+        age: '6 años',
+        gender: 'Macho',
+        image: 'img/perro5.jpg'
+      }
+];
